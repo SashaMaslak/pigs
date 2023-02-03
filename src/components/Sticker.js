812f354448e8b0c085b74a71f0bfb47dffce4fc9
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-02-03 10:25:34 +0200

    123

--- src/components/Sticker.js
+++ src/components/Sticker.js
@@ -1,33 +1,20 @@
 import { Component } from 'react';
 
+// export class Sticker extends Component = ({ sticker: { img, label }, onSelect }) => {
+
 export class Sticker extends Component {
-  // state = {
-  //   isOpen: false,
-  // };
-
-  // toggleLabel = () => {
-  //   this.setState(prevState => ({ isOpen: !prevState.isOpen }));
-  // };
-
   render() {
     const {
       onSelect,
-      onToggle,
       sticker: { img, label },
-      isSelected,
     } = this.props;
     return (
       <div
-        style={{ border: '1px solid black', padding: 8, borderRadius: 8 }}
-        onClick={() => {
-          onSelect(label);
-          onToggle(img);
-          // this.toggleLabel();
-        }}
+        onClick={() => onSelect(label)}
+        style={{ border: '1px solid black', padding: 12, borderRadius: 8 }}
       >
-        <img src={img} alt={label} width={120} />
-        {/* {this.state.isOpen && <p>{label}</p>} */}
-        {isSelected && <p>{label}</p>}
+        <img src={img} alt={label} width={160} />
+        <p>{label}</p>
       </div>
     );
   }
